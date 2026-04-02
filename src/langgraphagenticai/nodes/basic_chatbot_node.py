94cdb770bce9bc067687d527ccba08461f9c5f37
Sanket Sawarkar <sanket.sawarkar@alignedautomation.com>
2026-04-02 17:05:40 +0530

    fourth  commit

---
+++ src/langgraphagenticai/nodes/basic_chatbot_node.py
@@ -0,0 +1,12 @@
+from src.langgraphagenticai.state.state import State 
+
+class BasicChatbotNode:
+    def __init__(self,model):
+        self.llm=model
+        
+    def process(self,state:State):
+          """
+           Processes the input state and generates a chatbot response.
+        """  
+          return {"messages": self.invoke(state['messages'])}
+     
---
+++ src/langgraphagenticai/state/state.py
@@ -0,0 +1,14 @@
+from typing_extensions import TypedDict,List
+from langgraph.graph.message import add_messages
+from typing import Annotated
+
+
+class State(TypedDict):
+    """ 
+    Represent the structure of the state used in graph
+
+    Args:
+        TypedDict (_type_): _description_
+    """
+    
+    messages:Annotated[List,add_messages]
